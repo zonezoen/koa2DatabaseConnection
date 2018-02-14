f3abab777ea9986c3b29d92bfc2b7910588a83e9
zone <842221695@qq.com>
2018-02-14 14:46:06 +0800

    update code

--- routes/redis.js
+++ routes/redis.js
@@ -1,39 +1,31 @@
 const router = require('koa-router')()
 const config = require('../config/config')
-const Redis = require('ioredis');
 const redisUtil = require('./redisutil')
 
 
 router.prefix('/redis')
 
 // Redis ioredis 增删改查
-router.get('/index', async (ctx, next) => {
-    ctx.body = 'this is index page'
-})
-router.get('/add1', async (ctx, next) => {
-    redisUtil.setCommand(123456, {name: 'feifeiyu'})  //增
-    ctx.body = 'this is add page'
-
+router.get('/add1', async (ctx, next) => {//增
+    let data = await redisUtil.set(123456, {name: 'this message set by zone'})
+    ctx.body = 'this is add page . data ==> ' + data
 })
 
-router.get('/delete1', async (ctx, next) => {
-    redisUtil.delCommand(123456) //删
-    ctx.body = 'this is delete page'
-
+router.get('/delete1', async (ctx, next) => {//删
+    let data = await redisUtil.del(123456).then(response => {
+        console.log(response)
+    })
+    ctx.body = 'this is delete page . data ==> ' + data
 })
 
 router.get('/update1', async (ctx, next) => {
-    redisUtil.setCommand(123456, {name: 'feifeiyu3'}) //改
-    ctx.body = 'this is update page'
-
+    let data = await redisUtil.set(123456, {name: 'another message update by zone'}) //改
+    ctx.body = 'this is update page . data ==> ' + data
 })
 
-router.get('/query1', async (ctx, next) => {
-    let data = await redisUtil.getCommand(123456)
-    ctx.body = data
-    //查
-    // ctx.body = 'this is query page'
-
+router.get('/query1', async (ctx, next) => {//查
+    let data = await redisUtil.get(123456)
+    ctx.body = 'this is query page . data ==> ' + data
 })
 
 module.exports = router
--- routes/redisutil.js
+++ routes/redisutil.js
@@ -3,49 +3,22 @@
  */
 
 const redisConn = require('../db/redisConnection')
+const PREFIX = 'test_'
 class RedisTest {
     constructor() {
         this.redis = redisConn();
     }
 
-    // connToredis() {  //创建连接对象
-    //     return new Promise((resolve, reject) => {
-    //         if(this.redis) {
-    //             resolve(true) //已创建
-    //         } else {
-    //             redisConn().then(resp =>  {
-    //                 this.redis = resp
-    //                 resolve(true) }
-    //             ).catch(err => { reject(err) })
-    //         }
-    //     })
-    // }
-    setCommand(id, data, expire) {  //增/改
+    set(id, data, expire) {  //增/改
         if (expire === null || expire === undefined) {
-            this.redis.set(`test-${id}`, JSON.stringify(data)).then(resp => {
-                console.log('set', resp)
-                console.log('save success')
-
-            }).catch(err => {
-                console.log('err', err)
-                console.log('save fail')
-
-            })
-        } else {
-            this.redis.set(`test-${id}`, JSON.stringify(data), 'ex', expire).then(resp => {  //ex 为过期时间，单位为 秒
-                console.log('set', resp)
-                console.log('save success')
-
-            }).catch(err => {
-                console.log('err', err)
-                console.log('save fail')
-
-            })
+            this.redis.set(`${PREFIX}${id}`, JSON.stringify(data))
+        } else {//ex 为过期时间，单位为 秒
+            return this.redis.set(`test-${id}`, JSON.stringify(data), 'ex', expire)
         }
     }
 
-    getCommand(id) {  //查
-        return this.redis.get(`test-${id}`)
+    get(id) {  //查
+        return this.redis.get(`${PREFIX}${id}`)
         //     .then(resp => {
         //     console.log('get', resp)
         //     console.log('query success')
@@ -56,14 +29,12 @@ class RedisTest {
         // })
     }
 
-    delCommand(id) {
-        this.redis.del(`test-${id}`).then(resp => {
-            console.log('resp', resp)
-        })
+    del(id) {
+        return this.redis.del(`${PREFIX}${id}`)
     }
 
-    multiCommand(id, data) {
-        this.redis.multi().set(`test-${id}`, JSON.stringify(data))
+    multi(id, data) {
+        this.redis.multi().set(`${PREFIX}${id}`, JSON.stringify(data))
             .get(`test-${id}`).exec((err, resp) => {
             if (err) {
                 console.log('save fail')
@@ -77,26 +48,3 @@ class RedisTest {
 }
 let redisUtil = new RedisTest();
 module.exports = redisUtil
-//调用上面的对象，实现redis操作
-// let redisTest = new RedisTest()  //实例对象
-// redisTest.connToredis().then(resp => {  //连接成功后，进行redis操作
-//     if(resp) {
-//         redisTest.setCommand(123456, {name: 'feifeiyu'})  //增
-//         redisTest.getCommand(123456) //查
-//         redisTest.setCommand(123456, {name: 'feifeiyu3'}) //改
-//         redisTest.getCommand(123456)//查
-//         redisTest.delCommand(123456) //删
-//         redisTest.getCommand(123456) //差
-//         redisTest.multiCommand(123457, {name: 'feifeiyu2'})
-//     }
-// }).catch(err => {
-//     console.log('err', err)
-// })
-// setTimeout(() => { //再进行一次redis操作
-//     redisTest.connToredis().then(resp => {
-//         redisTest.setCommand(123458, {name: 'feifeiyu'}, 1)  //过期时间 1秒
-//         setTimeout(() => {  //等待过期
-//             redisTest.getCommand(123458) //过期返回结果为null
-//         }, 2000)
-//     })
-// }, 2000)
